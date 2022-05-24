61c6953da5aa505ebb009c4f07f7c66358599027
Jana Pascual <jacpascual@gmail.com>
2022-05-24 15:23:35 -0400

    Corrects error in updateCat function in App.jsx

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -55,11 +55,12 @@ function App() {
 
   const updateToy = async (toyData) => {}
 
-  const deleteCat = async (id) => {}
-
-  const deleteToy = async (id) => {
+  const deleteCat = async (id) => {
     await catService.deleteOne(id)
     setCats(cats.filter(cat => cat.id !== parseInt(id)))
+  }
+
+  const deleteToy = async () => {
   }
 
   const handleLogout = () => {
